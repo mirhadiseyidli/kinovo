96c9a91ade9b4f383f84bc8d1c7e8ccc212c0d1c
Mirhadi Seyidli <mirhadi.seyidli@Mirhadi-Seyidlis-MacBook-Pro.attlocal.net>
2025-06-12 18:44:07 -0700

    updates

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const categoryRoutes = require('./routes/categoryRoutes');
-const { initializeCategories } = require('./controllers/categoryController');
-
-// Initialize categories
-initializeCategories().catch(console.error);
-
-// Routes
-app.use('/api', categoryRoutes); 
